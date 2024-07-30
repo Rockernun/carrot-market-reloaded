027cfdddb13e9eaff85a1073c32e523057293108
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2024-07-30 15:42:26 +0900

    #3.8 Lists and Animations
    리스트를 활용한 홀수/짝수 번째 요소에 스타일 주기

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,19 @@
 export default function Home() {
   return (
-    <main className="bg-gray-100 h-screen flex items-center justify-center p-5 ">
+    <main className="bg-gray-100 sm:bg-red-100 md:bg-green-100 lg:bg-cyan-100 h-screen flex items-center justify-center p-5 ">
       <div className="flex flex-col gap-2 bg-white shadow-lg p-5 rounded-3xl w-full max-w-sm ">
-        <input
-          className="w-full rounded-full h-12 bg-gray-100 pl-5 outline-none ring ring-transparent focus:ring-orange-500 focus:ring-offset-2 transition-shadow placeholder:drop-shadow"
-          type="text"
-          placeholder="Search here..."
-        />
-        <button className="bg-black bg-opacity-50 text-white py-2 rounded-full active:scale-90 transition-transform font-medium outline-none">
-          Search
-        </button>
+        {["Nico", "Me", "You", "Yourself"].map((person, index) => (
+          <div
+            key={index}
+            className="flex items-center gap-5 even:bg-cyan-100 odd:bg-gray-100 p-2.5 rounded-xl"
+          >
+            <div className="size-7 bg-blue-400 rounded-full" />
+            <span className="text-lg font-medium">{person}</span>
+            <div className="size-6 bg-red-500 text-white flex items-center justify-center rounded-full">
+              <span>{index}</span>
+            </div>
+          </div>
+        ))}
       </div>
     </main>
   );
