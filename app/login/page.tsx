0eb93fb3de765467e08feeb4a6e3177083997eb8
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2024-08-03 23:07:53 +0900

    #5.0 Route Handlers
    route.ts라는 파일을 생성하면 HTTP 요청을 받아서 JSON 같은 형식으로 반환하거나 사용자를 이동시킬 수 있다. 단, route.ts 파일은 UI를 렌더링하지는 않는다.
    
    실행 결과를 확인해보면 잘 반환되는 것을 확인할 수 있다. 굳이 내가 request의 메소드를 검증할 필요 없이 함수 이름을 GET, POST라고 정해주면 알아서 요청을 보낼 수 있다.

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,8 +1,20 @@
+"use client";
+
 import FormButton from "@/components/form-btn";
 import FormInput from "@/components/form-input";
 import SocialLogin from "@/components/social-login";
 
 export default function LogIn() {
+  const onClick = async () => {
+    const response = await fetch("/api/users", {
+      method: "POST",
+      body: JSON.stringify({
+        username: "nico",
+        password: "1234",
+      }),
+    });
+    console.log(await response.json());
+  };
   return (
     <div className="flex flex-col gap-10 py-8 px-6">
       <div className="flex flex-col gap-2 *:font-medium">
@@ -10,16 +22,23 @@ export default function LogIn() {
         <h2 className="text-xl">Log in with email and password</h2>
       </div>
       <form className="flex flex-col gap-3">
-        <FormInput required placeholder="Email" type="email" errors={[]} />
         <FormInput
-          required
+          required={false}
+          placeholder="Email"
+          type="email"
+          errors={[]}
+        />
+        <FormInput
+          required={false}
           placeholder="Password"
           type="password"
           errors={[]}
         />
-        <FormButton loading={false} text="Log in" />
-        <SocialLogin />
       </form>
+      <span onClick={onClick}>
+        <FormButton loading={false} text="Log in" />
+      </span>
+      <SocialLogin />
     </div>
   );
 }
