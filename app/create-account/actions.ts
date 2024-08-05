a7d6a139a84d76322f5591054a0e6146ca7d4083
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2024-08-05 21:51:29 +0900

    #6.2 Refinement
    주석과 같이, refine 안에 작성한 함수가 true를 리턴하면 문제가 없는 것이고, false를 리턴하면 에러가 있는 것이고 에러 메시지를 띄울 수 있다.
    
    Zod가 checkUsername 함수를 받아서 호출하고, 검증해야 할 username을 인자로 넣어 준다.
    
    하지만 여기서 password와 confirm_password가 일치하는지 여부를 처리할 때, refine을 사용하면 password, confirm_password 각각을 받아서 refine하게 된다. password와 confirm_password를 받아서 함께 검증하기 위해 Object 전체를 refine했다. 그럼 Object 안의 모든 필드를 validation할 수 있는 것이다.
    
    이 경우도 동일하게 refine 안에 작성한 함수가 false를 반환하면 에러 메시지가 표시돼야 하는데, 문제는 어디에 나타나느냐다. 콘솔로 찍어보면 Zod가 해당 에러는 특정 필드에 속하지 않는다고 판단한다. 에러가 발생하면, 그것이 Global 에러라고 생각하는 것이다.
    
    지금 원하는 건, 에러 메시지가 confirm_password에 나타나야 하는데, 이를 위해 Zod에게 해당 에러가 confirm_password라는 특정 필드에 속한 것이라고 정확히 알려줘야 한다. 이때 path 옵션을 사용하면 된다.
    
    물론 path는 field 이름과 동일해야 한다. 만약 path를 지우면 Zod는 에러가 form 전체와 관련된 공통 에러라고 판단할 것이다.
    
    *refine은 2개의 인수를 받는다.
    1. 유효성 검사 함수
    2. 몇 가지 옵션
    -message : 에러 메시지 지정
    -path : 에러 경로 지정
    -params : 에러 발생 시 메시지를 커스텀하기 위해 사용되는 객체

--- app/create-account/actions.ts
+++ app/create-account/actions.ts
@@ -1,14 +1,40 @@
 "use server";
 import { z } from "zod";
 
-const formSchema = z.object({
-  username: z.string().min(3).max(10),
-  email: z.string().email(),
-  password: z.string().min(10),
-  confirm_password: z.string().min(10),
-});
+//  potato가 포함된 username 처리 함수
+function checkUsername(username: string) {
+  return !username.includes("potato");
+}
 
-//  Form에서 모든 item(object)을 가져온다.
+//  비밀번호 일치 여부를 처리하는 함수
+const checkPasswords = ({
+  password,
+  confirm_password,
+}: {
+  password: string;
+  confirm_password: string;
+}) => password === confirm_password;
+
+const formSchema = z
+  .object({
+    username: z
+      .string({
+        invalid_type_error: "Username must be a string!",
+        required_error: "Where is my username?",
+      })
+      .min(3, "Way too short!")
+      .max(10, "That is too long!")
+      .refine(checkUsername, "No potatoes allowed..."),
+    //  refine 안에 작성한 함수가 true를 리턴하면 문제가 없고, false를 리턴하면 문제가 있다는 것이다. false를 반환할 경우, 유저에게 에러 메시지가 표시된다.
+    email: z.string().email(),
+    password: z.string().min(10),
+    confirm_password: z.string().min(10),
+  })
+  .refine(checkPasswords, {
+    message: "Both passwords should be the same...",
+    path: ["confirm_password"], //  이런 식으로 에러의 주인이 누구인지 명시해줘야 한다.
+  });
+
 export async function createAccount(prevState: any, formData: FormData) {
   const data = {
     username: formData.get("username"),
@@ -16,13 +42,6 @@ export async function createAccount(prevState: any, formData: FormData) {
     password: formData.get("password"),
     confirm_password: formData.get("confirm_password"),
   };
-  /*
-  try {
-    formSchema.safeParse(data);
-  } catch (e) {
-    console.log(e);
-  }
-    */
 
   const result = formSchema.safeParse(data);
   if (!result.success) {
