9ca79d7f815ed0c032e3e3cc8cd5aae2ab874edd
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2024-08-07 16:04:12 +0900

    #Code Challenge 2
    이메일 형식을 필터링하는 checkEmailForm 함수를 작성하고 refine 메서드를 이용해 @zod.com 형식이 아닌 경우, 해당 에러 메시지를 출력하도록 했다.
    
    마찬가지로, Username의 길이가 5만큼 되지 않는 경우, refine 메서드로 에러 메시지를 출력하도록 했다.
    
    마지막으로, Password도 최소 길이를 10으로 설정하고, 정규표현식을 이용해서 숫자가 적어도 1개 이상 포함하고, 그렇지 않으면 에러 메시지를 출력하도록 regex 메서드를 사용해서 처리했다.

--- app/login/actions.ts
+++ app/login/actions.ts
@@ -4,24 +4,40 @@ import {
   PASSWORD_MIN_LENGTH,
   PASSWORD_REGEX,
   PASSWORD_REGEX_ERROR,
+  USERNAME_MIN_LENGTH,
 } from "@/lib/constants";
 import { z } from "zod";
 
 function checkPasswords(password: string) {
-  return password === "12345";
+  return password === "1234567890";
 }
 
 function correctPasswords(password: string) {
-  return password !== "12345";
+  return password !== "1234567890";
+}
+
+function checkEmailForm(email: string) {
+  return email.includes("@zod.com");
 }
 
 const formSchema = z.object({
-  email: z.string().email().toLowerCase(),
-  username: z.string().toLowerCase(),
+  email: z
+    .string()
+    .email()
+    .toLowerCase()
+    .refine(checkEmailForm, "Only @zod.com emails are allowed"),
+  username: z
+    .string({
+      required_error: "Please write your username...",
+    })
+    .min(USERNAME_MIN_LENGTH, "Username should be at least 5 characters long.")
+    .toLowerCase(),
   password: z
     .string({
       required_error: "Password is required...",
     })
+    .min(PASSWORD_MIN_LENGTH, "Password should be at least 10 characters long.")
+    .regex(PASSWORD_REGEX, PASSWORD_REGEX_ERROR)
     .refine(checkPasswords, "Wrong Password...")
     .refine(correctPasswords, "Welcome Back!"),
 });
@@ -36,12 +52,6 @@ export async function Login(prevState: any, formData: FormData) {
   if (!result.success) {
     return result.error.flatten();
   } else {
-    /*
-  if (data.password === "12345") {
-    return ["Welcome!"];
-  }
-  이 구문을 지우니 fieldErrors 오류가 사라졌다.
-  */
     console.log(result.data);
   }
 }
