67000252e475d6baadb95bdd8f4c88fe35e55a31
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2024-08-10 22:48:08 +0900

    #8.6 superRefine
    이미 존재하는 사용자명과 이메일로 계정을 만들려고 시도해보면, refinement가 사용자명, 이메일, 비밀번호 등 모든 영역에서 발생하고 있는 것을 확인할 수 있다.
    
    위 상황은 checkUniqueUsername이 데이터베이스를 호출하고, checkUniqueEmail도 데이터베이스를 호출해서 발생하는 일이다. 지금은 쿼리 크기가 크지 않기 때문에 괜찮지만 검사를 일찍 멈추는 방법에 대해서도 알아봤다. 예를 들어, Username부터 유효하지 않는다면 다른 검사를 실행조차 하지 않도록 하는 거다. 이런 걸 위한 superRefine이라는 메서드가 존재한다. refine과 동일하게 동작하지만 다른 모든 검사들을 중단시켜 더 빨리 돌아올 수 있다.
    
    사용하기 위해 Object를 refine했다. superRefine도 함수를 실행하지만, 첫 번째 인자는 내가 검사할 값이다. 내가 현재 refine 하고 있는 값이 될 거다. 두 번째 인자는 RefinementCtx다. 쉽게 말하면 에러 묶음인데, Zod는 기본적으로 데이터를 검사하고 RefinementCtx에 에러를 추가한다. 그리고 내가 원한다면 에러를 ctx에 추가할 수 있다.
    
    그리고 Zod에게 에러를 발생시킨 필드는 Username이라는 것을 알려줘야 한다. 그러기 위해 path안에 username을 넣었다. 이렇게 하면 에러가 username input 밑에 추가된다. 콘솔을 확인해보면, 이 에러가 formErrors에서 username error로 바뀐 것을 확인할 수 있다. 이제 선순위의 검사들이 실패한다면 다른 검사들은 처리하지 않도록 했다.
    
    fatal Issue를 만들고, NEVER를 return하면 superRefine 뒤에 다른 refine이 있어도 그것들은 실행되지 않는다. superRefine이 NEVER를 return하는데 거기에 fatal Issue가 있다면 그 후의 다른 refine들은 실행되지 않을 거다.
    
    이게 바로 검사를 미리 중단시키는 방법이다. 해야할 건, 단지 fatal이 true인 Issue를 만들고 NEVER를 return만 하면 된다. 그러면 그 뒤의 어떤 refine도 실행되지 않을 것이다.

--- app/create-account/actions.ts
+++ app/create-account/actions.ts
@@ -7,8 +7,6 @@ import {
 } from "@/lib/constants";
 import db from "@/lib/db";
 import { z } from "zod";
-import { getIronSession } from "iron-session";
-import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 import getSession from "@/lib/session";
 
@@ -24,38 +22,6 @@ const checkPasswords = ({
   confirm_password: string;
 }) => password === confirm_password;
 
-const checkUniqueUserName = async (username: string) => {
-  const user = await db.user.findUnique({
-    where: {
-      username,
-    },
-    select: {
-      id: true, //  필요한 id 데이터만 갖도록 한다.
-    },
-  });
-  //  refine을 사용하기 위해 리턴값을 True/False로 반환하도록 했다.
-  /*if (user) {
-    return false;
-  } else {
-    return true;
-  }*/
-  return !Boolean(user); //  이렇게 작성할 수도 있다.
-};
-
-//  SELECT id FROM User WHERE username="?"
-
-const checkUniqueEmail = async (email: string) => {
-  const user = await db.user.findUnique({
-    where: {
-      email,
-    },
-    select: {
-      id: true,
-    },
-  });
-  return !Boolean(user);
-};
-
 const formSchema = z
   .object({
     username: z
@@ -66,21 +32,53 @@ const formSchema = z
       .toLowerCase()
       .trim()
       //.transform((username) => `🔥 ${username} 🔥`)
-      .refine(checkUsername, "No potatoes allowed...")
-      .refine(checkUniqueUserName, "This username is already taken"),
-    email: z
-      .string()
-      .email()
-      .toLowerCase()
-      .refine(
-        checkUniqueEmail,
-        "There is an account already registered with this email..."
-      ),
+      .refine(checkUsername, "No potatoes allowed..."),
+
+    email: z.string().email().toLowerCase(),
+
     password: z
       .string()
       .min(PASSWORD_MIN_LENGTH)
       .regex(PASSWORD_REGEX, PASSWORD_REGEX_ERROR),
     confirm_password: z.string().min(PASSWORD_MIN_LENGTH),
+  })
+  .superRefine(async ({ username }, ctx) => {
+    const user = await db.user.findUnique({
+      where: {
+        username,
+      },
+      select: {
+        id: true,
+      },
+    });
+    if (user) {
+      ctx.addIssue({
+        code: "custom",
+        message: "This username is already taken...",
+        path: ["username"],
+        fatal: true,
+      });
+      return z.NEVER;
+    }
+  })
+  .superRefine(async ({ email }, ctx) => {
+    const user = await db.user.findUnique({
+      where: {
+        email,
+      },
+      select: {
+        id: true,
+      },
+    });
+    if (user) {
+      ctx.addIssue({
+        code: "custom",
+        message: "This E-mail is already taken...",
+        path: ["email"],
+        fatal: true,
+      });
+      return z.NEVER;
+    }
   })
   .refine(checkPasswords, {
     message: "Both passwords should be the same...",
@@ -97,6 +95,7 @@ export async function createAccount(prevState: any, formData: FormData) {
 
   const result = await formSchema.safeParseAsync(data);
   if (!result.success) {
+    console.log(result.error.flatten());
     return result.error.flatten();
   } else {
     const hashedPassword = await bcrypt.hash(result.data.password, 12);
