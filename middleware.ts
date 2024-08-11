18bf719875251ea5edb48f3e265c494d36bd0655
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2024-08-12 01:18:45 +0900

    #8.10 Matcher
    matcher를 사용하면 middleware를 필터링해서 특정 경로들에서만 실행되도록 설정할 수 있다.

--- middleware.ts
+++ middleware.ts
@@ -1,13 +1,10 @@
 import { error } from "console";
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-//  middleware가 request를 가로채서 profile 페이지로 가려는 request를 완전히 중단시킨 것이다.
-export function middleware(request: NextRequest) {
-  console.log(request.cookies.getAll()); //  이런 식으로 쿠키에 접근할 수도 있다.
-  //  console.log(cookies());
-  //  const session = await getSession(); 이런 식으로 세션으로 사용자의 로그인 여부도 확인 가능하다.
-  //  console.log(session);
-  if (request.nextUrl.pathname === "/profile") {
-    return Response.redirect(new URL("/", request.url));
-  }
+export async function middleware(request: NextRequest) {
+  console.log("hello");
 }
+
+export const config = {
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+};
