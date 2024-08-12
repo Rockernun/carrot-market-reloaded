e68ab70ba4eab001a6e7840149ce39e05f0bcba2
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2024-08-12 21:17:41 +0900

    #10.3 Product Component
    Product model에서 title 오타 수정.
    
    고구마 Product 데이터를 하나 생성하고, getProducts function을 만들었다.
    
    image를 inspect해보면, src는 product의 photo다. 그저 /goguma.jpg란 얘기다. Next가 image를 가져가서 많은 작업을 했는데, 우선 lazy load 하도록 만들었다. 이건 오직 사용자가 보고 있는 동안에만 load 된다는 소리다. 만약 수백 개의 product가 있다고 한다면 loading="lazy" 는 브라우저가 그 image를 전부 다운로드하지 않게 한다. 오직 사용자가 image를 보려고 할 때만 다운로드할 거다. 만약 매우 큰 리스트가 있고 스크롤을 내린다면 모든 image의 리스트가 load 되지는 않았다고 확신할 수 있다. 이런 작업들은 Next에 의해 자동으로 추가된 것이다. 그리고 또 좋은 점은 srcset 속성도 추가되었다는 건데, 이 srcset는 다른 image를 보여줄 수 있도록 허용하는 속성이다. 다른 스크린 해상도일 때 말이다.
    
    1x의 일반 스크린을 위한 일반적인 픽셀 밀도가 있고, image가 조금의 변형과 quality는 75%로 압축되어 있다. 바로 옆은 스크린 2배의 2x 픽셀 밀도를 가지고 있다. color는 transparent로 image가 load 되는 동안 기본적으로 transparent(투명한) 박스, width는 200이고 height는 200이다. image가 load 하는 동안 placeholder 같은 transparent(투명성) 박스를 갖게 되는데 Layout Shift를 막아줄거다. 단지 내가 해야 할 건, image가 얼마나 큰지 알려주는 것이다.
    
    하지만 문제는 가끔 image가 픽셀 사이즈로 얼마나 커질지 모른다는 것이다. 대부분의 경우 얼마나 커질지 알고 있어야 하지만, 얼마나 큰지 모를 경우에는 옵션이 하나 있다. 그건 바로 width와 height을 없애고 fill 이라는 속성을 사용하는 것이다. 그럼 부모 element가 image로 가득 채워진다.

--- prisma/migrations/20240812025327_add_product/migration.sql
+++ prisma/migrations/20240812025327_add_product/migration.sql
@@ -1,7 +1,7 @@
 -- CreateTable
 CREATE TABLE "Product" (
     "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "titie" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
     "price" REAL NOT NULL,
     "description" TEXT NOT NULL,
     "photo" TEXT NOT NULL,
