73a163f21564a1145d18fa69452f2fca279c51a7
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2024-08-12 23:53:27 +0900

    #10.6 Image Hostnames
    현재 user와 password를 가지고 로그인했기 때문에 user는 avatar가 따로 없다. 그래서 임의로 깃허브의 프로필 사진 주소를 복사해서 붙여 넣었다.
    
    그럼 에러가 발생하는데,  Next가 웹사이트에 속한 아래 이미지들을 최적화하도록 설정하지 않았기 때문이다. Next 어플리케이션을 어디에 배포하느냐에 따라 다르지만, 이미지 최적화는 서버가 작동해야함을 의미한다. 압축이 일어나야 하고, 최적화가 일어나야 한다. 이건 어떤 방식으로든 비용이 발생함을 의미하는데, 그래서 기본적으로 악의적인 user들에 의해 너무 많은 비용을 지불하는 것을 방지하기 위해 어떤 URL 이미지를 최적화할지 결정해야 한다.
    
    어플리케이션에 이미지의 URL을 업로드하는 많은 사람들이 있다고 가정하자. 해당 이미지를 표시하는 경우, 누군가가 많은 이미지를 업로드했을 수 있고, 이는 내가 모든 이미지를 최적화해야함을 의미한다. 만약 image component를 사용하고 있다면, 만약 그게 user가 직접 제작한 content라면 난 아마 그 image component를 사용하고 싶지 않을 수 있다.
    
    그래서 현재 발생한 에러를 해결하기 위해 Next에게 특별히 해당 hostname의 이미지를 최적화해달라고 하는 것이다.

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        //  해당 도메인을 가진 이미지를 image component로 최적화
+        hostname: "avatars.githubusercontent.com",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
