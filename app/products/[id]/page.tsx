2a49ceefc4ff6a8518b6251dbf398944974c7bb9
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2024-08-14 23:51:26 +0900

    #11.0 Product Upload
    상품을 추가하는 버튼을 만들고, tab bar는 상품 추가 페이지에는 필요없기 때문에 tabs 그룹 안이 아닌, layout이 없는 products 폴더에 만들었다.
    
    label에는 for 속성이 있는데, React에서는 htmlFor 라고 한다. 현재 label에 for의 속성값은 photo라고 했고, input의 id 속성값도 photo로 놓아서 label을 클릭할 때마다 input이 focus된다. 기억하자. HTML에서 input에 id가 있으면, 그 input을 위한 label을 만들 수 있다.
    
    이제 사용자에게 미리보기를 보여주기 위해서 약간의 상호작용을 추가할거다. 이미지를 선택하고 open을 클릭하면 사용자가 업로드하려는 이미지를 보여주는 것이다.

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -7,8 +7,7 @@ import { formatToWon } from "@/lib/utils";
 import { UserIcon } from "@heroicons/react/24/solid";
 
 async function getIsOwner(userId: number) {
-  const session = await getSession(); //  사용자의 쿠키를 읽는다.
-  //  사용자의 쿠키에 있는 id가 제품을 업로드한 사용자의 id와 일치하는지 여부를 체크
+  const session = await getSession();
   if (session.id) {
     return session.id === userId;
   }
@@ -35,15 +34,14 @@ async function getProduct(id: number) {
 export default async function ProductDetail({
   params,
 }: {
-  params: { id: string }; //  URL로부터 파라미터를 가져온다. (/product 다음의 숫자 부분 params.id)
+  params: { id: string };
 }) {
-  const id = Number(params.id); //  숫자로 변경할 수 없는 id일 경우, 404 에러 처리
+  const id = Number(params.id);
   if (isNaN(id)) {
     return notFound();
   }
   const product = await getProduct(id);
   if (!product) {
-    //  제품이 존재하지 않으면 notFound 리턴
     return notFound();
   }
   const isOwner = await getIsOwner(product.userId);
